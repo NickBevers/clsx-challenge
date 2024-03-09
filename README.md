ac09caf838aa6a8ed486d08f6c958974d152a540
Nick Bevers <nick.bevers9@gmail.com>
2024-03-09 12:38:55 +0100

    feat: add own clsx version and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # clsx-challenge
+A personal challenge of mine to write the clsx package myself but making it as small and compact as possible.
+This code was tested according to the clsx test suite and passed all tests.
---
+++ clsx.js
@@ -0,0 +1,27 @@
+function toVal(m) {
+  let y,s = '';
+
+  if (typeof m === 'string' || typeof m === 'number') (s += m);
+  if (typeof m === 'object') {
+    if (Array.isArray(m)) {
+      m.forEach(y => y && typeof y !== 'boolean' && (s && (s += ' '), s += toVal(y)))
+    } else {
+      for (y in m) {
+        m[y] && (s && (s += ' '), s += y);
+      }
+    }
+  }
+
+  return s;
+}
+
+export function clsx() {
+  let t,x,s='';
+  for (t of arguments) {
+    t && (x = toVal(t)) && (s && (s += ' '), s += x);
+  }
+
+  return s;
+}
+
+export default clsx;
